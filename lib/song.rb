70f9ef94cc5c53058dff661b54f0e12a3c6ad3cd
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-08 16:30:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,33 @@
+require('pry')
 class Song
 
-  attr_accessor :artist, :title
+  attr_accessor :artist, :name
+  @@all = []
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
+    @@all.push(self)
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    song_info = filename.split(" - ")
+    new_song = Song.new(song_info[1])
+    new_song.artist_name = Artist.find_or_create_by_name(song_info[0])
+    binding.pry
+    new_song
+  end
+  
+  def artist_name= (artist_name)
+    found_artist = Artist.all.find{ |artist| artist.name == artist_name}
+    
+    if found_artist == nil
+      found_artist = Artist.new(artist_name)
+    end
+    found_artist.add_song(self)
+    found_artist.name
+  end
 end
