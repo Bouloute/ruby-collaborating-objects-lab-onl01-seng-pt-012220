b4f563f9d02213ea0bc5413ebed1f137f7263928
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-08 16:36:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(filename)
     song_info = filename.split(" - ")
     new_song = Song.new(song_info[1])
-    new_song.artist_name = Artist.find_or_create_by_name(song_info[0])
+    new_song.artist_name = song_info[0]
     binding.pry
     new_song
   end
